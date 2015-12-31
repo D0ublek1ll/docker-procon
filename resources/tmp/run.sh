dcde82d244bd20a0c06783a910af13da48794b84
Azerothian <admin@illisian.com.au>
2015-12-31 15:54:21 +1000

    bug fixes

--- resources/tmp/run.sh
+++ resources/tmp/run.sh
@@ -2,7 +2,7 @@
 /tmp/tmpl.sh /tmp/Configs/accounts.cfg PC > /procon/Configs/accounts.cfg
 /tmp/tmpl.sh /tmp/Configs/procon.cfg PC > /procon/Configs/procon.cfg
 /tmp/tmpl.sh /tmp/Configs/usage.xml PC > /procon/usage.xml
-mkdir -p "/procon/${PC_SERVER_IP}_${PC_SERVER_PORT}/"
-/tmp/tmpl.sh "/tmp/$PC_SERVER_TYPE/Server.cfg" PC > "/procon/${PC_SERVER_IP}_${PC_SERVER_PORT}/${PC_SERVER_IP}_${PC_SERVER_PORT}.cfg"
+mkdir -p "/procon/Configs/${PC_SERVER_IP}_${PC_SERVER_PORT}/"
+/tmp/tmpl.sh "/tmp/$PC_SERVER_TYPE/Server.cfg" PC > "/procon/Configs/${PC_SERVER_IP}_${PC_SERVER_PORT}/${PC_SERVER_IP}_${PC_SERVER_PORT}.cfg"
 cd /procon/
 mono PRoCon.Console.exe
--- resources/tmp/tmpl.sh
+++ resources/tmp/tmpl.sh
@@ -3,9 +3,9 @@ TEMPLATE_FILE=$1
 ENV_PREFIX=$2
 
 env | grep "^$ENV_PREFIX" | while IFS="=" read -r name value; do
-printf "s/%s/%s/\n" "${name}" "$value"
+printf "s/%s/%s/\n" "\$\{${name}\}" "$value"
 done > sed-script.txt
 
-cat sed-script.txt   # Optional
+#cat sed-script.txt   # Optional
 sed -f sed-script.txt "$TEMPLATE_FILE"
 rm sed-script.txt
