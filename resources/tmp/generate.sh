8885aa46088ec195594ad87dd7b8d00e534f9355
Azerothian <admin@illisian.com.au>
2015-12-31 14:42:37 +1000

    fixes generate

--- resources/tmp/generate.sh
+++ resources/tmp/generate.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
-./tmpl.sh ./Configs/accounts.cfg PC > /procon/Configs/accounts.cfg
-./tmpl.sh ./Configs/procon.cfg PC > /procon/Configs/procon.cfg
-./tmpl.sh ./Configs/usage.xml PC > /procon/usage.xml
+/tmp/tmpl.sh /tmp/Configs/accounts.cfg PC > /procon/Configs/accounts.cfg
+/tmp/tmpl.sh /tmp/Configs/procon.cfg PC > /procon/Configs/procon.cfg
+/tmp/tmpl.sh /tmp/Configs/usage.xml PC > /procon/usage.xml
 mkdir -p "/procon/$PC_SERVER_IP_$PC_SERVER_PORT/"
-./tmpl.sh "./$PC_SERVER_TYPE/Server.cfg" PC > "/procon/$PC_SERVER_IP_$PC_SERVER_PORT/$PC_SERVER_IP_$PC_SERVER_PORT.cfg"
+/tmp/tmpl.sh "/tmp/$PC_SERVER_TYPE/Server.cfg" PC > "/procon/$PC_SERVER_IP_$PC_SERVER_PORT/$PC_SERVER_IP_$PC_SERVER_PORT.cfg"
