a827734743c319e35a871f83108d4275ca085b6c
D0ublek1ll <ik@mickdejong.nl>
2020-09-17 17:37:01 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 cd /home/container
 
+#Show current mono version
+mono --version
+
 # Replace Startup Variables
 MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
 echo ":/home/container$ ${MODIFIED_STARTUP}"
